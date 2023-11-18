029ab79e3279ed12d299c3dbf90a989c51a1effb
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-11-19 00:11:21 +0530

    new: customer review section added

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
 
 const CustomerReviews = () => {
-  return <div></div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What our <span className="text-coral-red ">Customers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Here genuine stories from our satisfied customers about their
+        exceptional experience with us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} {...review} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
