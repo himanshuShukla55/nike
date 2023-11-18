3be551a16fd296793cc51322c19ce2d869bd032d
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-11-18 23:29:48 +0530

    new: services section added

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
