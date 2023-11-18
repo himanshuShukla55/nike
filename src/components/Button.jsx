8d9039a06a7dbc3250c4870bad05539b29679205
Himanshu Shukla <itachiuchiha@Himanshus-MacBook-Air.local>
2023-11-19 00:28:45 +0530

    new: contact us section done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
@@ -13,7 +14,7 @@ const Button = ({
         backgroundColor
           ? backgroundColor + " " + textColor + " " + borderColor
           : " bg-coral-red  text-white border-coral-red"
-      } rounded-full`}
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
       {iconURL && (
